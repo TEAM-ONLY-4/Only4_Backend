0589000733ecba2a90c0cd7f380df7f7428feace
HyeongseoShin <tlsgudtj123@gmail.com>
2026-01-13 20:12:22 +0900

    initial commit

---
+++ src/main/java/com/ureca/only4_be/Only4BeApplication.java
@@ -0,0 +1,13 @@
+package com.ureca.only4_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Only4BeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Only4BeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ureca/only4_be/Only4BeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ureca.only4_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Only4BeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
