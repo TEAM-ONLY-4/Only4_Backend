017a128c3928dfbd82aadca5a05e6ec14dd40b73
meisseoyeon <dkssud1191@naver.com>
2026-01-16 22:44:22 +0900

    job,step기능추가

---
+++ src/main/java/com/ureca/only4_be/batch/jobs/notification/NotificationJobConfig.java
@@ -0,0 +1,26 @@
+package com.ureca.only4_be.batch.jobs.notification;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+public class NotificationJobConfig {
+
+    private final JobRepository jobRepository;
+    private final Step notificationStep;
+
+    @Bean
+    public Job notificationJob() {
+        return new JobBuilder("notificationJob", jobRepository)
+                .start(notificationStep) // Step 시작
+                // .incrementer(new RunIdIncrementer()) //테스트시 코드
+                .build();
+    }
+}
