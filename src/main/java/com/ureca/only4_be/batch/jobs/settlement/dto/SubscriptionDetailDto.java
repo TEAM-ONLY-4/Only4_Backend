c0c6bffd0ee8ac84caec67b060328a94f31a4c35
YeongHyeonHeo <djdjfug@naver.com>
2026-01-18 21:04:55 +0900

    [Feat]: SubscriptionDetailDto 생성

---
+++ src/main/java/com/ureca/only4_be/batch/jobs/settlement/dto/SubscriptionDetailDto.java
@@ -0,0 +1,16 @@
+package com.ureca.only4_be.batch.jobs.settlement.dto;
+
+import com.ureca.only4_be.domain.subscription.Subscription;
+import com.ureca.only4_be.domain.subscription_discount.SubscriptionDiscount;
+import com.ureca.only4_be.domain.subscription_usage.SubscriptionUsage;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import java.util.List;
+
+@Getter
+@AllArgsConstructor
+public class SubscriptionDetailDto {
+    private Subscription subscription;          // 구독 정보 (상품 정보 포함됨)
+    private List<SubscriptionUsage> usages;     // 이 구독의 사용 내역
+    private List<SubscriptionDiscount> discounts; // 이 구독의 할인 내역 (할인 정책 포함됨)
+}
