751c5c28135504a6e77d44ef2374b2f090e74a6b
YeongHyeonHeo <djdjfug@naver.com>
2026-01-18 17:44:17 +0900

    [Feat]: Job Config 뼈대 생성

--- src/main/java/com/ureca/only4_be/batch/jobs/settlement/SettlementJobConfig.java
+++ src/main/java/com/ureca/only4_be/batch/jobs/settlement/SettlementJobConfig.java
@@ -1,4 +1,24 @@
 package com.ureca.only4_be.batch.jobs.settlement;
 
+import com.ureca.only4_be.batch.jobs.settlement.step.SettlementStepConfig;
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
 public class SettlementJobConfig {
+
+    private final JobRepository jobRepository;
+    private final SettlementStepConfig settlementStepConfig; // Step 설정 가져오기
+
+    @Bean
+    public Job settlementJob() {
+        return new JobBuilder("settlementJob", jobRepository)
+                .start(settlementStepConfig.settlementStep()) // Step 실행
+                .build();
+    }
 }
