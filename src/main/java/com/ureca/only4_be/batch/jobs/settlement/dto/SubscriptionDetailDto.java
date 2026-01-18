a4702324d1616e64f36c627d065a3b321251dc49
YeongHyeonHeo <djdjfug@naver.com>
2026-01-18 23:02:44 +0900

    [Feat]: SubscriptionDetailDto에 tvSpec 추가

--- src/main/java/com/ureca/only4_be/batch/jobs/settlement/dto/SubscriptionDetailDto.java
+++ src/main/java/com/ureca/only4_be/batch/jobs/settlement/dto/SubscriptionDetailDto.java
@@ -1,5 +1,6 @@
 package com.ureca.only4_be.batch.jobs.settlement.dto;
 
+import com.ureca.only4_be.domain.product.TvSpec;
 import com.ureca.only4_be.domain.subscription.Subscription;
 import com.ureca.only4_be.domain.subscription_discount.SubscriptionDiscount;
 import com.ureca.only4_be.domain.subscription_usage.SubscriptionUsage;
@@ -16,7 +17,8 @@ public class SubscriptionDetailDto {
     private List<SubscriptionUsage> usages;     // 이 구독의 사용 내역
     private List<SubscriptionDiscount> discounts; // 이 구독의 할인 내역 (할인 정책 포함됨)
 
-    // 스펙 정보들 (없으면 null 들어감)
+    // 스펙 정보들 (해당하는 것만 채워지고 나머지는 null)
     private MobilePlanSpec mobilePlanSpec;
     private AddonSpec addonSpec;
+    private TvSpec tvSpec;
 }
