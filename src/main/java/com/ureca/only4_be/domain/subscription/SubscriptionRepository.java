24aab826b811eb4c81d44c08e820d08d5855f99f
YeongHyeonHeo <djdjfug@naver.com>
2026-01-18 21:21:39 +0900

    [Feat]: SubscriptionRepository 생성

---
+++ src/main/java/com/ureca/only4_be/domain/subscription/SubscriptionRepository.java
@@ -0,0 +1,14 @@
+package com.ureca.only4_be.domain.subscription;
+
+import com.ureca.only4_be.domain.member.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface SubscriptionRepository extends JpaRepository<Subscription, Long> {
+    // 상품(Product) 정보까지 한 방에 가져옴
+    @Query("SELECT s FROM Subscription s JOIN FETCH s.product WHERE s.member = :member AND s.subscriptionStatus = 'ACTIVE'")
+    List<Subscription> findAllByMemberWithProduct(@Param("member") Member member);
+}
