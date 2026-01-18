0b11a2c61468e223250db5312951dc36643dc7dd
YeongHyeonHeo <djdjfug@naver.com>
2026-01-18 21:29:59 +0900

    [Feat]: SubscriptionDiscountRepository 생성

---
+++ src/main/java/com/ureca/only4_be/domain/subscription_discount/SubscriptionDiscountRepository.java
@@ -0,0 +1,15 @@
+package com.ureca.only4_be.domain.subscription_discount;
+
+import com.ureca.only4_be.domain.subscription.Subscription;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface SubscriptionDiscountRepository extends JpaRepository<SubscriptionDiscount, Long> {
+    // 할인 정책(DiscountPolicy) 정보까지 한 방에 가져옴 (할인율 확인용)
+    // IN 절을 사용하여 여러 구독의 할인을 한 번에 조회
+    @Query("SELECT sd FROM SubscriptionDiscount sd JOIN FETCH sd.discountPolicy WHERE sd.subscription IN :subscriptions")
+    List<SubscriptionDiscount> findAllBySubscriptionIn(@Param("subscriptions") List<Subscription> subscriptions);
+}
