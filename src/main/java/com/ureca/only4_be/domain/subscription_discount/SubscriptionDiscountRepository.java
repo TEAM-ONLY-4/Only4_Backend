c9a5be42b21f5cc04b5ed30bc97cf5a1a7051483
YeongHyeonHeo <djdjfug@naver.com>
2026-01-19 03:30:44 +0900

    [Feat]: BillItemRepository 생성

--- src/main/java/com/ureca/only4_be/domain/subscription_discount/SubscriptionDiscountRepository.java
+++ src/main/java/com/ureca/only4_be/domain/subscription_discount/SubscriptionDiscountRepository.java
@@ -4,9 +4,11 @@ import com.ureca.only4_be.domain.subscription.Subscription;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface SubscriptionDiscountRepository extends JpaRepository<SubscriptionDiscount, Long> {
     // 할인 정책(DiscountPolicy) 정보까지 한 방에 가져옴 (할인율 확인용)
     // IN 절을 사용하여 여러 구독의 할인을 한 번에 조회
