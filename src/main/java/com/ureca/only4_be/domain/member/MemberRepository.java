d8e0b0fb7cfe6cf0bd2a2789cd2dfed7d41fe558
YeongHyeonHeo <djdjfug@naver.com>
2026-01-19 20:48:25 +0900

    [REFACTOR]: Reader의 청구서가 없는 회원 조회 -> MemberRepository로 이동

--- src/main/java/com/ureca/only4_be/domain/member/MemberRepository.java
+++ src/main/java/com/ureca/only4_be/domain/member/MemberRepository.java
@@ -1,8 +1,20 @@
 package com.ureca.only4_be.domain.member;
 
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import java.time.LocalDate;
 
-@Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    // 청구서가 없는 회원만 페이징으로 가져오기 (Processor 검사 대체)
+    @Query("SELECT m FROM Member m " +
+            "WHERE NOT EXISTS (" +
+            "   SELECT 1 FROM Bill b " +
+            "   WHERE b.member = m " +
+            "   AND b.billingYearMonth = :targetDate" +
+            ")")
+    Slice<Member> findMembersWithoutBill(@Param("targetDate") LocalDate targetDate, Pageable pageable);
 }
