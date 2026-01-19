39c3a4e8efd87b3479ad947ec5a372d785825565
meisseoyeon <dkssud1191@naver.com>
2026-01-19 11:13:13 +0900

    query,topic,dto변경

--- src/main/java/com/ureca/only4_be/batch/jobs/notification/reader/NotificationItemReader.java
+++ src/main/java/com/ureca/only4_be/batch/jobs/notification/reader/NotificationItemReader.java
@@ -14,6 +14,7 @@ import org.springframework.data.domain.Sort;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
@@ -25,7 +26,7 @@ public class NotificationItemReader {
     private final EntityManagerFactory entityManagerFactory;
 
     @Bean
-    @StepScope // Job이 실행될 때마다 새로 생성
+    @StepScope
     public JpaPagingItemReader<Bill> notificationReader(
             @Value("#{jobParameters['requestDate']}") String requestDateStr){
 
@@ -35,11 +36,13 @@ public class NotificationItemReader {
 
         int targetDay = targetDate.getDayOfMonth(); //일 -> 유저의 설정날짜와 비교
         LocalTime nowTime = LocalTime.now(); // 시 -> 방해금지 시간 비교
+        LocalDate currentBillingMonth = targetDate.withDayOfMonth(1);//이번 달 청구서만 조회하기 위한 기준
 
         Map<String, Object> parameters = new HashMap<>(); //쿼리문에 들어갈 변수 저장 위치(파라미터로 넘기는 게 보안상 안전)
-        parameters.put("status", BillSendStatus.BEFORE_SENT);
+        parameters.put("statusList", Arrays.asList(BillSendStatus.BEFORE_SENT, BillSendStatus.FAILED));
         parameters.put("notificationDayOfMonth", (short) targetDay);
         parameters.put("currentTime", nowTime);
+        parameters.put("billingMonth", currentBillingMonth);
 
         return new JpaPagingItemReaderBuilder<Bill>() //bill객체 읽어오는 reader
                 .name("notificationReader") //Execution Context
@@ -48,8 +51,8 @@ public class NotificationItemReader {
                 .queryString(
                         "SELECT b FROM Bill b " +
                         "JOIN FETCH b.member m " + // N+1 방지
-                        "WHERE b.billSendStatus = :status " +
-                        "AND m.notificationDayOfMonth = :notificationDayOfMonth " +
+                        "WHERE b.billSendStatus = :statusList " +
+                        "AND m.notificationDayOfMonth <= :notificationDayOfMonth " +// 지난 날짜들 까지 조회 --- 실패처리 ---
                         "AND (" +
                         //  금지 시간을 설정하지 않은 사람은 무조건 통과 (NULL 체크)
                         "   (m.doNotDisturbStartTime IS NULL OR m.doNotDisturbEndTime IS NULL) " +
@@ -61,7 +64,8 @@ public class NotificationItemReader {
                         // 밤샘 케이스 (예: 23:00 ~ 07:00 금지) -> 07시~23시 사이여야 함
                         "   (m.doNotDisturbStartTime > m.doNotDisturbEndTime " +
                         "       AND (:currentTime > m.doNotDisturbEndTime AND :currentTime < m.doNotDisturbStartTime)) " +
-                        ")"
+                        ") " +
+                        "ORDER BY m.notificationDayOfMonth ASC, b.id ASC" // (5) PagingReader는 정렬 필수
                 )
                 .parameterValues(parameters)
                 .build();
