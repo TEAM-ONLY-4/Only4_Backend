3958fed45bc2b847543d11c8c01d8022c2a736e6
HyeongseoShin <tlsgudtj123@gmail.com>
2026-01-19 16:14:38 +0900

    [FIX]: 이메일 Producer Event DTO NotificationRequest로 변경

--- src/main/java/com/ureca/only4_be/kafka/event/EmailSendRequestEvent.java
+++
@@ -1,7 +0,0 @@
-package com.ureca.only4_be.kafka.event;
-
-public record EmailSendRequestEvent (
-        Long memberId,
-        Long billId
-)
-{}
--- src/main/java/com/ureca/only4_be/kafka/service/EmailKafkaProducer.java
+++ src/main/java/com/ureca/only4_be/kafka/service/EmailKafkaProducer.java
@@ -1,6 +1,6 @@
 package com.ureca.only4_be.kafka.service;
 
-import com.ureca.only4_be.kafka.event.EmailSendRequestEvent;
+import com.ureca.only4_be.batch.jobs.notification.dto.NotificationRequest;
 import com.ureca.only4_be.kafka.properties.KafkaTopicsProperties;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -14,17 +14,17 @@ public class EmailKafkaProducer {
     private final KafkaTemplate<String, Object> kafkaTemplate;
     private final KafkaTopicsProperties kafkaTopicsProperties;
 
-    public void send(EmailSendRequestEvent emailSendRequestEvent) {
+    public void send(NotificationRequest notificationRequest) {
         String topic = kafkaTopicsProperties.emailRequest();
 
-        kafkaTemplate.send(topic, emailSendRequestEvent)
+        kafkaTemplate.send(topic, notificationRequest)
                 .whenComplete((result, ex) -> {
                     if(ex != null) {
                         log.error("[Email Producer] 발행 실패. topic={}, memberId={}, billId={}, error={}",
-                                topic, emailSendRequestEvent.memberId(), emailSendRequestEvent.billId(), ex.getMessage());
+                                topic, notificationRequest.getMemberId(), notificationRequest.getBillId(), ex.getMessage());
                     } else {
                         log.info("[Email Producer] 발행 성공. topic={}, memberId={}, billId={}",
-                                topic, emailSendRequestEvent.memberId(), emailSendRequestEvent.billId());
+                                topic, notificationRequest.getMemberId(), notificationRequest.getBillId());
                     }
                 });
         }
