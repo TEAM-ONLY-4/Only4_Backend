7b461874382b9ccb09ec691815af7aa03d55d7e1
YeongHyeonHeo <djdjfug@naver.com>
2026-01-19 20:59:40 +0900

    [FEAT]: OneTimePurchaseRepository IN절 쿼리로 수정

--- src/main/java/com/ureca/only4_be/domain/one_time_purchase/OneTimePurchaseRepository.java
+++ src/main/java/com/ureca/only4_be/domain/one_time_purchase/OneTimePurchaseRepository.java
@@ -1,12 +1,15 @@
 package com.ureca.only4_be.domain.one_time_purchase;
 
-import com.ureca.only4_be.domain.member.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import java.util.List;
 
 @Repository
 public interface OneTimePurchaseRepository extends JpaRepository<OneTimePurchase, Long> {
-    List<OneTimePurchase> findAllByMember(Member member);
+
+    /**
+     * [배치용 Bulk Fetch 쿼리]
+     * 특별한 연관관계 Fetch가 필요 없다면 메소드 이름만으로 IN 절 쿼리 생성 가능.
+     */
+    List<OneTimePurchase> findAllByMemberIdIn(List<Long> memberIds);
 }
