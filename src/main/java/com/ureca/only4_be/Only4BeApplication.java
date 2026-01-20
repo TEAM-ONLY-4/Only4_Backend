b916673d4879a2ebd7e2443000f5473ca84297d9
YeongHyeonHeo <djdjfug@naver.com>
2026-01-20 13:27:57 +0900

    [FEAT]: Only4BeApplication에 @EnableScheduling 추가

--- src/main/java/com/ureca/only4_be/Only4BeApplication.java
+++ src/main/java/com/ureca/only4_be/Only4BeApplication.java
@@ -3,7 +3,9 @@ package com.ureca.only4_be;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 @ConfigurationPropertiesScan
 public class Only4BeApplication {
